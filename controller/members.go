9fc9b6062bc5f4b6f9efc25a9639f778de9b3da2
DocWho22 <710980175@qq.com>
2022-02-13 22:38:50 +0800

    新加了GetMember函数和ListMember函数的雏形

--- controller/members.go
+++ controller/members.go
@@ -11,12 +11,14 @@ import (
 	"strings"
 )
 
+// CreateMember 创建新用户
+// TODO 检查当前登录用户是否是管理员还没做
 func CreateMember(c *gin.Context) {
 	var Response _type.CreateMemberResponse
 	var Request _type.CreateMemberRequest
 	// 获取JSON参数
 	if err := c.ShouldBind(&Request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "error"})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Connection error"})
 	}
 	// 将参数绑定到Member里
 	NewMember := _type.Member{
@@ -28,7 +30,7 @@ func CreateMember(c *gin.Context) {
 	}
 	// 判断参数是否合法
 	if modules.IsMemberParamValid(NewMember) {
-		// 参数合法（Username在后面)
+		// 参数合法（Username检查在后面)
 		Response.Code = _type.OK
 	} else {
 		// 参数不合法
@@ -39,13 +41,12 @@ func CreateMember(c *gin.Context) {
 	// 添加记录
 	if Response.Code == _type.OK {
 		db := DB.MysqlDB
-		tx := db.Create(&NewMember)
+		result := db.Create(&NewMember)
 		// 判断用户名是否重复
-
-		if tx.Error == nil {
+		if result.Error == nil {
 			// Username 没有重复，成功创建
 			Response.Data.UserID = strconv.Itoa(int(NewMember.UserID))
-		} else if Error := fmt.Sprintf("/v", tx.Error); strings.Contains(Error, "Error 1062") {
+		} else if Error := fmt.Sprintf("/v", result.Error); strings.Contains(Error, "Error 1062") {
 			// Username 重复
 			Response.Code = _type.UserHasExisted
 			Response.Data.UserID = ""
@@ -54,4 +55,56 @@ func CreateMember(c *gin.Context) {
 		}
 	}
 	c.JSON(http.StatusOK, Response)
+}
+
+// GetMember 获取当前登录用户信息
+// TODO 用户未登录 还没做
+func GetMember(c *gin.Context) {
+	var Response _type.GetMemberResponse
+	var Request _type.GetMemberRequest
+	// 获取JSON参数
+	if err := c.ShouldBind(&Request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Connection error"})
+	}
+	// 全局变量DB赋值
+	db := DB.MysqlDB
+	// 查询UserID对应的记录
+	var MemberInfo _type.Member
+	result := db.Find(&MemberInfo, Request.UserID)
+	// 传入TMember类型参数
+	ResponseMemberInfo := _type.TMember{
+		UserID:   strconv.Itoa(int(MemberInfo.UserID)),
+		Nickname: MemberInfo.Nickname,
+		Username: MemberInfo.Username,
+		UserType: MemberInfo.UserType,
+	}
+	// 检查错误
+	if result.RowsAffected == 0 {
+		// 用户不存在：检查 ErrRecordNotFound 错误
+		Response.Code = _type.UserNotExisted
+		ResponseMemberInfo.UserID = ""
+		ResponseMemberInfo.UserType = -1
+	} else if MemberInfo.Status == _type.Deleted {
+		// 用户已删除 TODO 还没检查
+		Response.Code = _type.UserHasDeleted
+	} else {
+		// 一切正常
+		Response.Code = _type.OK
+	}
+	// 传入Response
+	Response.Data = ResponseMemberInfo
+	// 用户未登录 还没做
+
+	// 返回Response
+	c.JSON(http.StatusOK, Response)
+}
+
+func ListMember(c *gin.Context) {
+	//var Response _type.GetMemberListResponse
+	var Request _type.GetMemberListRequest
+	// 获取JSON参数
+	if err := c.ShouldBind(&Request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Connection error"})
+	}
+
 }
